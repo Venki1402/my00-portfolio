bff2a9fda4c436fa3f7c0c28cb2d01cfb0e9d820
Venki1402 <saivenkatesh.alampally@gmail.com>
2024-05-27 11:35:15 +0530

    initial commit

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./NavBar.css";
+import logo from '../../assets/Untitled.svg';
+import { Link } from 'react-scroll';
+
+const NavBar = () => {  
+  return (
+    <nav className="navbar">
+        <img src={logo} alt="logo" className="logo"/>
+        <div className="menu-bar">
+            <Link className="menu-bar-item">Home</Link>
+            <Link className="menu-bar-item">About</Link>
+            <Link className="menu-bar-item">Skills</Link>
+            <Link className="menu-bar-item">Projects</Link>
+            <Link className="menu-bar-item">Contact</Link>
+        </div>
+    </nav>
+  );
+}
+
+export default NavBar;
